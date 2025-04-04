2a9120e159d3a6683363c5941352a3258fa991a9
rhuanpk <dev@rhuanpk.net>
2025-04-04 20:33:58 -0300

    progii: add bibliotecas_maven

---
+++ bibliotecas_maven/src/main/java/net/rhuanpk/bibliotecas_maven/classes/Bar.java
@@ -0,0 +1,19 @@
+package net.rhuanpk.bibliotecas_maven.classes;
+
+public class Bar {
+
+	private boolean foo;
+
+	public Bar(boolean foo) {
+		setFoo(foo);
+	}
+
+	public boolean isFoo() {
+		return foo;
+	}
+
+	public void setFoo(boolean foo) {
+		this.foo = foo;
+	}
+
+}
---
+++ bibliotecas_maven/src/main/java/net/rhuanpk/bibliotecas_maven/classes/Foo.java
@@ -0,0 +1,19 @@
+package net.rhuanpk.bibliotecas_maven.classes;
+
+public class Foo {
+
+	private boolean bar;
+
+	public Foo(boolean bar) {
+		setBar(bar);
+	}
+
+	public boolean isBar() {
+		return bar;
+	}
+
+	public void setBar(boolean bar) {
+		this.bar = bar;
+	}
+
+}
