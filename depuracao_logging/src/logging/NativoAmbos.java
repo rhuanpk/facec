e96d4b1beca2f841e5bd1db9bf76ce635d0b1019
rhuanpk <dev@rhuanpk.net>
2025-05-09 19:09:55 -0300

    progii: update depuracao_logging

--- depuracao_logging/src/logging/NativoAmbos.java
+++ depuracao_logging/src/logging/NativoAmbos.java
@@ -1,9 +1,31 @@
 package logging;
 
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class NativoAmbos {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SecurityException, IOException {
+
+		Logger logger = Logger.getLogger(NativoArquivo.class.getName());
+		LogManager.getLogManager().reset();
+
+		FileHandler fileHandler = new FileHandler("/tmp/tmp/file.log");
+		ConsoleHandler consoleHandler = new ConsoleHandler();
+
+		fileHandler.setFormatter(new SimpleFormatter());
+		logger.addHandler(consoleHandler);
+		logger.addHandler(fileHandler);
+
+		logger.info("mensagem de INFORMAÇÃO");
+		logger.fine("mensagem de TRACE");
+		logger.warning("mensagem de AVISO");
+		logger.severe("mensagem de ERRO");
+		logger.config("mensagem de CONFIGURAÇÃO");
 
 	}
 
--- depuracao_logging/src/logging/NativoTerminal.java
+++ depuracao_logging/src/logging/NativoTerminal.java
@@ -1,10 +1,23 @@
 package logging;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class NativoTerminal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub		
-		
+
+		Logger logger = Logger.getLogger(NativoTerminal.class.getName());
+
+		logger.log(Level.ALL, "level ALL");
+		logger.log(Level.CONFIG, "level CONFIG");
+		logger.log(Level.FINE, "level FINE");
+		logger.log(Level.FINER, "level FINER");
+		logger.log(Level.FINEST, "level FINEST");
+		logger.log(Level.INFO, "level INFO");
+		logger.log(Level.SEVERE, "level SEVERE");
+		logger.log(Level.WARNING, "level WARNING");
+
 	}
 
 }
